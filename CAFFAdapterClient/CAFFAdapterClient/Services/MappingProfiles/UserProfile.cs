fb5e882a721cf38165ac6e4d5b3a19fe532cb51f
Máté Pércsi <percsim@outlook.com>
2021-11-20 17:44:20 +0100

    [backend] json patch profile

---
+++ CAFFAdapterClient/CAFFAdapterClient/Services/MappingProfiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CAFFAdapterClient.DataTransferObjects.Account;
+using CAFFAdapterClient.Domain;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace CAFFAdapterClient.Services.MappingProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<JsonPatchDocument<EditUserDto>, JsonPatchDocument<User>>();
+        }
+    }
+}
